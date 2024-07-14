937fb0118aae9aa4974aeca36d18e4fc9c9a8c9f
bhavesh3007 <chouhanbhavesh007@gmail.com>
2024-07-14 23:43:46 +0530

    added utility and middlewares

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ dotenv.config({
     path: './env'
 })
 connetDB()
-
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("Mongo DB connrction failed !!!", err)
+})
 
 
 /*
